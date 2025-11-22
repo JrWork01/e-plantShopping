1d717b3d52db20e4bfc7c61b870981d0f4c8ec8e
JrWork01 <jpeguerogarcia@learning.eoi.es>
2025-11-22 11:35:19 -0500

    Saving changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 import cartReducer from './CartSlice';
@@ -9,6 +10,9 @@ function ProductList({ onHomeClick }) {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
 
+    const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.items);
+
     const handleAddToCart = (product) => {
         dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
       
@@ -16,6 +20,10 @@ function ProductList({ onHomeClick }) {
           ...prevState, // Spread the previous state to retain existing entries
           [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
         }));
+      };
+
+      const calculateTotalQuantity = () => {
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
       };
 
     const plantsArray = [
